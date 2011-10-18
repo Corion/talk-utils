7fde89b3e6dd14c6aa509e7c4e494ad07c5d38fd
Max Maischein <corion@corion.net>
2011-10-18 16:22:36 +0200

    Slight update of code

--- svg2ani.pl
+++ svg2ani.pl
@@ -1,26 +1,29 @@
 #!/usr/bin/perl -w
 use strict;
 use lib 'c:/Dokumente und Einstellungen/Corion/Desktop/Talks/lib';
-use lib './lib';
 use SVG::Layers;
-use File::DosGlob qw(bsd_glob);
+use File::Glob qw(bsd_glob);
 use File::Spec;
 
-@ARGV = map { glob $_ } @ARGV;
+@ARGV = map { bsd_glob $_ } @ARGV;
 
 my $inkscape = "C:\\Programme\\Inkscape\\inkscape.exe";
 
+$|++;
+
 for my $file (@ARGV) {
-    print "$file\n";
+    print "$file";
     my @output = SVG::Layers::get_layers($file);
     
     for my $file (@output) {
         $file = File::Spec->rel2abs($file,'.');
         (my $png = $file) =~ s/\.svg/.png/i;
+        $png =~ s/\bdiagrams\b/images/i;
         my $cmd = qq{$inkscape --export-png "$png" "$file"};
         system($cmd) == 0
             or die "Couldn't convert $file to $png: $!/$?\n";
-        print "=> $png\n";
+        print ".";
+        unlink $file or warn "Couldn't remove '$file': $!";
     }
-    
+    print "\n";
 }
