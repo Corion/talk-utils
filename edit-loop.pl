bfe67776e797a7ed62f6689cde410c1a9182cd28
Max Maischein <corion@corion.net>
2011-10-18 15:31:03 +0200

    Allow direct restarting of slide show

---
+++ edit-loop.pl
@@ -0,0 +1,38 @@
+#!perl -w
+use Win32::FileNotify;
+#use HTML::Display;
+#use HTML::Display::MozRepl;
+use WWW::Mechanize::Firefox;
+
+use Getopt::Long;
+GetOptions(
+    'v|verbose' => \my $verbose,
+);
+
+sub status($) {
+    print "$_[0]\n"
+        if($verbose);
+};
+
+my ($file,@cmd) = @ARGV;
+my $watch = Win32::FileNotify->new($file);
+my $mech = WWW::Mechanize::Firefox->new();
+
+(my $html = $file) =~ s/\.pod$/\.html/i;
+
+status "Loading $html";
+$mech->get_local($html, basedir => '.');
+while (1) {
+    $watch->wait;
+    system(@cmd);
+
+    # Remember what slide of the slideshow was shown
+    my ($page,$type) = $mech->eval_in_page('snum');
+
+    status "Reloading $html";
+    $mech->get_local($html, basedir => '.');
+    
+    $page ||= '0';
+    status "Showing slide $page";
+    $mech->eval_in_page("go($page);");
+};
