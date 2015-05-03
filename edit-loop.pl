0a7d2e02a4d8d47a08267fa16a1b0517e46f18e5
Max Maischein <corion@corion.net>
2015-05-03 19:51:33 +0200

    Make loop better

--- edit-loop.pl
+++ edit-loop.pl
@@ -3,10 +3,12 @@ use Win32::FileNotify;
 #use HTML::Display;
 #use HTML::Display::MozRepl;
 use WWW::Mechanize::Firefox;
+use File::Glob qw(bsd_glob);
 
 use Getopt::Long;
 GetOptions(
     'v|verbose' => \my $verbose,
+    'f|file:s' => \my @files,
 );
 
 sub status($) {
@@ -15,15 +17,29 @@ sub status($) {
 };
 
 my ($file,@cmd) = @ARGV;
+push @files, $file;
+@files = map { bsd_glob $_ } @files;
+$file= $files[0];
+
+if(0 == @cmd and -f 'Makefile') {
+    @cmd = 'dmake';
+};
+
 my $watch = Win32::FileNotify->new($file);
 my $mech = WWW::Mechanize::Firefox->new();
 
 (my $html = $file) =~ s/\.pod$/\.html/i;
 
-status "Loading $html";
-$mech->get_local($html, basedir => '.');
+if( -f $html ) {
+    status "Loading $html";
+    $mech->get_local($html, basedir => '.');
+};
+
+status "Watchcing $file";
 while (1) {
-    $watch->wait;
+    if( -f $html ) {
+        $watch->wait;
+    };
     system(@cmd);
 
     # Remember what slide of the slideshow was shown
