95e839950389642f1f6f74e96eff583f2672ed71
Max Maischein <corion@corion.net>
2015-05-03 19:52:23 +0200

    Export to a directory

--- svg2ani.pl
+++ svg2ani.pl
@@ -5,22 +5,38 @@ use lib './lib';
 use SVG::Layers;
 use File::DosGlob qw(bsd_glob);
 use File::Spec;
+use File::Basename;
 
+use Getopt::Long;
+GetOptions(
+    'o|outdir:s' => \my $outdir,
+);
 @ARGV = map { glob $_ } @ARGV;
 
 my $inkscape = "C:\\Programme\\Inkscape\\inkscape.exe";
-
-for my $file (@ARGV) {
+for my $file (@ARGV) {
+    my $ts = -M $file;
     print "$file\n";
     my @output = SVG::Layers::get_layers($file);
+    if( $outdir ) {
+        $outdir = File::Spec->rel2abs($outdir,'.');
+    };
     
     for my $file (@output) {
         $file = File::Spec->rel2abs($file,'.');
         (my $png = $file) =~ s/\.svg/.png/i;
-        my $cmd = qq{$inkscape --export-png "$png" "$file"};
-        system($cmd) == 0
-            or die "Couldn't convert $file to $png: $!/$?\n";
-        print "=> $png\n";
+        
+        if( $outdir ) {
+            $png = File::Spec->catfile( $outdir, basename( $png ) );
+        };
+        
+        if( ! -e $png or $ts > -M $file ) {
+            my $cmd = qq{$inkscape --export-png "$png" "$file"};
+            print "[$cmd]\n";
+            system($cmd) == 0
+                or die "Couldn't convert $file to $png: $!/$?\n";
+            print "=> $png\n";
+        };
     }
     
 }
