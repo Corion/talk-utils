ddf2cf9a0bd8ec2fedd83f9a8eb0909900f0cff2
Max Maischein <corion@corion.net>
2018-08-04 13:14:02 +0200

    Switch to WWW::Mechanize::Chrome

--- edit-loop.pl
+++ edit-loop.pl
@@ -1,8 +1,11 @@
 #!perl -w
 use Win32::FileNotify;
-#use HTML::Display;
-#use HTML::Display::MozRepl;
-use WWW::Mechanize::Firefox;
+use WWW::Mechanize::Chrome;
+use Log::Log4perl ':easy';
+use File::Glob ':bsd_glob';
+use Path::Class 'file';
+
+Log::Log4perl->easy_init($ERROR);
 
 use Getopt::Long;
 GetOptions(
@@ -15,24 +18,35 @@ sub status($) {
 };
 
 my ($file,@cmd) = @ARGV;
-my $watch = Win32::FileNotify->new($file);
-my $mech = WWW::Mechanize::Firefox->new();
 
-(my $html = $file) =~ s/\.pod$/\.html/i;
+if( ! @cmd ) {
+    @cmd = 'gmake';
+};
+
+$file = file(bsd_glob $file);
+my $watch = Win32::FileNotify->new($file->absolute);
+my $mech = WWW::Mechanize::Chrome->new(
+    headless => 0,
+);
+
+(my $html = $file->absolute) =~ s/\.pod$/\.html/i;
 
 status "Loading $html";
-$mech->get_local($html, basedir => '.');
+$mech->get_local("$html");
 while (1) {
     $watch->wait;
+    
     system(@cmd);
 
     # Remember what slide of the slideshow was shown
-    my ($page,$type) = $mech->eval_in_page('snum');
+    #my ($page,$type) = $mech->eval_in_page('snum');
+    my $url = $mech->uri;
+    (my $page) = $url =~ /(\d+)$/;
 
     status "Reloading $html";
-    $mech->get_local($html, basedir => '.');
+    $mech->get_local("$html");
     
     $page ||= '0';
-    status "Showing slide $page";
-    $mech->eval_in_page("go($page);");
+    status "Showing slide $url";
+    $mech->eval_in_page("go($page)");
 };
